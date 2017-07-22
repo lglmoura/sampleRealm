4d209962cbfefc11cdac1a13e774adae72560471
Luiz Gustavo <lglmoura@gmail.com>
2017-07-22 10:59:31 -0300

    Alterando o nome do banco

--- app/src/main/java/pooa20171/iff/br/samplerealm/persistence/BDConfig.java
+++ app/src/main/java/pooa20171/iff/br/samplerealm/persistence/BDConfig.java
@@ -17,7 +17,7 @@ public class BDConfig  extends Application {
             Realm.init(getApplicationContext());
 
             RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
-            builder.name("teste.realm");
+            builder.name("biblioteca.realm");
             builder.schemaVersion(0);
             builder.deleteRealmIfMigrationNeeded();
             RealmConfiguration realmConfiguration = builder.build();
